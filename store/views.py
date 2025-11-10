026911ef1080d9ff16070ce4c41da27afeca136d
NicolasReynaga <sus719793@gmail.com>
2025-11-10 07:39:16 -0800

    Modificamos items

--- store/views.py
+++ store/views.py
@@ -2,31 +2,48 @@ from django.shortcuts import render
 from .models import Item, Category
 from django.shortcuts import get_object_or_404
 
-#Create your views here.
+
+
+
+# Create your views here.
 def home(request):
     items = Item.objects.filter(is_sold=False)
-    categories= Category.objects.all()
+    categories = Category.objects.all()
+
+
+
 
     context = {
         'items': items,
         'categories': categories
     }
-    return render(request, 'store/home.html', context)
+    return render(request,'store/home.html', context)
+
+
+
 
 def contact(request):
     context = {
-        'msg':'Quires otros productos contactame!'
+        'msg': 'Quieres otros productos contactame!'
     }
-
     return render(request, 'store/contact.html', context)
+
+
+
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    related_items = Item.objects.filter(category=item.Category,is_sold=False).exclude(pk=pk)[0:3]
+    related_items = Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
+
+
+
 
     context={
         'item': item,
         'related_items': related_items
     }
 
-    return render(request, 'store/item.html', context)
+
+
+
+    return render(request,'store/item.html', context)
