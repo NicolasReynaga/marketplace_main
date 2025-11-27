5b3e40f1c3b3bb005a55d8c73a5115349d30424f
NicolasReynaga <cuentatemp729@gmail.com>
2025-11-27 07:54:54 -0800

    ngrok prueba

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(ydc-ev9@h4ofb*t!2xw765e3s_bzk4v2sdk3qa5dp&f*ibtbu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alana-monomolecular-shaunna.ngrok-free.dev']
 
 LOGIN_URL = '/store/login/'
 LOGIN_REDIRECT_URL = '/'
